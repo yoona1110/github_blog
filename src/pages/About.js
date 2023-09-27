9ff0505556d5446a6ca13fb66647b06e8be497ea
yoona1110 <gjdbsdk18@icloud.com>
2023-09-28 04:25:50 +0900

    Feat: 각 메뉴 별 title 구현

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,31 @@
+import styled from "styled-components";
+
 const About = () => {
     return (
-        <div> About </div>
+        <Container>
+            <Title> About </Title>
+            <Line/>
+        </Container>
     );
 };
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.p`
+    color: #9190B4;
+    font-size: 22px;
+    font-weight: 700;
+    line-height: normal;
+    margin: 0 0 5px 10px;
+`;
+
+const Line = styled.hr`
+    width: 852px;
+    border: 0.5px dotted #9190B4;
+    margin-bottom: 19px;
+`;
+
 export default About;
